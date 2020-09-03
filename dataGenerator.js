6330eb16ec1440852bdd26060e7d45d9a7b87008
Devon Poston <dev.poston@gmail.com>
2020-09-03 00:17:05 -0700

    added lots of css and html

--- dataGenerator.js
+++ dataGenerator.js
@@ -7,10 +7,12 @@
 window.streams = {};
 streams.home = [];
 streams.users = {};
-streams.users.shawndrost = [];
-streams.users.sharksforcheap = [];
-streams.users.mracus = [];
-streams.users.douglascalhoun = [];
+streams.users.greatwhite = [];
+streams.users.bullshark = [];
+streams.users.hammerheadshark = [];
+streams.users.tigershark = [];
+streams.users.nurseshark = [];
+streams.users.jaws = [];
 window.users = Object.keys(streams.users);
 
 // utility function for adding tweets to our data structures
@@ -28,10 +30,10 @@ var randomElement = function(array){
 
 // random tweet generator
 var opening = ['just', '', '', '', '', 'ask me how i', 'completely', 'nearly', 'productively', 'efficiently', 'last night i', 'the president', 'that wizard', 'a ninja', 'a seedy old man'];
-var verbs = ['downloaded', 'interfaced', 'deployed', 'developed', 'built', 'invented', 'experienced', 'navigated', 'aided', 'enjoyed', 'engineered', 'installed', 'debugged', 'delegated', 'automated', 'formulated', 'systematized', 'overhauled', 'computed'];
+var verbs = ['ate', 'swam', 'jumped', 'bite', 'bit', 'eat', 'bask', 'sank', 'swim', 'enjoyed', 'engineered', 'installed', 'debugged', 'delegated', 'automated', 'formulated', 'systematized', 'overhauled', 'computed'];
 var objects = ['my', 'your', 'the', 'a', 'my', 'an entire', 'this', 'that', 'the', 'the big', 'a new form of'];
-var nouns = ['cat', 'koolaid', 'system', 'city', 'worm', 'cloud', 'potato', 'money', 'way of life', 'belief system', 'security system', 'bad decision', 'future', 'life', 'pony', 'mind'];
-var tags = ['#techlife', '#burningman', '#sf', 'but only i know how', 'for real', '#sxsw', '#ballin', '#omg', '#yolo', '#magic', '', '', '', ''];
+var nouns = ['shark', 'turtle', 'dolphin', 'human', 'diver', 'license plate', 'fish', 'squid', 'way of life', 'belief system', 'security system', 'bad decision', 'future', 'life', 'pony', 'mind'];
+var tags = ['#sharklife', '#findingnemo', '#justkeepswimming', '#bigbite', '#finproblems', '#sharkattack', '#jaws', '#cantstopwontstop...eating', '#howamitypingthis', '#hellateeth', '', ''];
 
 var randomMessage = function(){
   return [randomElement(opening), randomElement(verbs), randomElement(objects), randomElement(nouns), randomElement(tags)].join(' ');
@@ -71,3 +73,4 @@ var writeTweet = function(message){
   tweet.created_at = new Date();
   addTweet(tweet);
 };
+console.log(window.streams);
